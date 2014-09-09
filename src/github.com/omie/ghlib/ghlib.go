a39b60f255f9b3e17447b7aaa902d8652bbedf33
Omie <intelomkar@gmail.com>
2014-09-10 01:58:50 +0530

    missed underscores in exported names. fixed using camelCase

--- src/github.com/omie/ghlib/ghlib.go
+++ src/github.com/omie/ghlib/ghlib.go
@@ -35,21 +35,21 @@ type GhLimit struct {
 type GhUser struct {
     Login string `json:"login"`
     Id int `json:"id"`
-    Avatar_url string `json:"avatar_url"`
-    Gravatar_id string `json:"gravatar_id"`
+    AvatarUrl string `json:"avatar_url"`
+    GravatarId string `json:"gravatar_id"`
     Url string `json:"url"`
-    Html_url string `json:"html_url"`
-    Followers_url string `json:"followers_url"`
-    Following_url string `json:"following_url"`
-    Gists_url string `json:"gists_url"`
-    Starred_url string `json:"starred_url"`
-    Subscriptions_url string `json:"subscriptions_url"`
-    Organizations_url string `json:"organizations_url"`
-    Repos_url string `json:"repos_url"`
-    Events_url string `json:"events_url"`
-    Received_events_url string `json:"received_events_url"`
+    HtmlUrl string `json:"html_url"`
+    FollowersUrl string `json:"followers_url"`
+    FollowingUrl string `json:"following_url"`
+    GistsUrl string `json:"gists_url"`
+    StarredUrl string `json:"starred_url"`
+    SubscriptionsUrl string `json:"subscriptions_url"`
+    OrganizationsUrl string `json:"organizations_url"`
+    ReposUrl string `json:"repos_url"`
+    EventsUrl string `json:"events_url"`
+    ReceivedEvents_url string `json:"received_events_url"`
     Usertype string `json:"type"`
-    Site_admin bool `json:"site_admin"`
+    SiteAdmin bool `json:"site_admin"`
     Name string `json:"name"`
     Company string `json:"company"`
     Blog string `json:"blog"`
@@ -57,81 +57,81 @@ type GhUser struct {
     Email string "email"
     Hireable bool `json:"hireable"`
     Bio string `json:"bio"`
-    Public_repos int `json:"public_repos"`
-    Public_gists int `json:"public_gists"`
+    PublicRepos int `json:"public_repos"`
+    PublicGists int `json:"public_gists"`
     Followers int `json:"followers"`
     Following int `json:"following"`
-    Created_at string `json:"created_at"`
-    Updated_at string `json:"updated_at"`
+    CreatedAt string `json:"created_at"`
+    UpdatedAt string `json:"updated_at"`
 }
 
 type GhRepository struct {
     Id int `json:"id"`
     Name string `json:"name"`
-    Full_name string `json:"full_name"`
+    FullName string `json:"full_name"`
     Owner GhUser `json:"owner"`
     Private bool `json:"private"`
-    Html_url string `json:"html_url"`
+    HtmlUrl string `json:"html_url"`
     Description string `json:"description"`
     Fork bool `json:"fork"`
     Url string `json:"url"`
-    Forks_url string `json:"forks_url"`
-    Keys_url string `json:"keys_url"`
-    Collaborators_url string `json:"collaborators_url"`
-    Teams_url string `json:"teams_url"`
-    Hooks_url string `json:"hooks_url"`
-    Issue_events_url string `json:"issue_events_url"`
-    Events_url string `json:"events_url"`
-    Assignees_url string `json:"assignees_url"`
-    Branches_url string `json:"branches_url"`
-    Tags_url string `json:"tags_url"`
-    Blobs_url string `json:"blobs_url"`
-    Git_tags_url string `json:"git_tags_url"`
-    Git_refs_url string `json:"git_refs_url"`
-    Trees_url string `json:"trees_url"`
-    Statuses_url string `json:"statuses_url"`
-    Languages_url string `json:"languages_url"`
-    Stargazers_url string `json:"stargazers_url"`
-    Contributors_url string `json:"contributors_url"`
-    Subscribers_url string `json:"subscribers_url"`
-    Subscription_url string `json:"subscription_url"`
-    Commits_url string `json:"commits_url"`
-    Git_commits_url string `json:"git_commits_url"`
-    Comments_url string `json:"comments_url"`
-    Issue_comment_url string `json:"issue_comment_url"`
-    Contents_url string `json:"contents_url"`
-    Compare_url string `json:"compare_url"`
-    Merges_url string `json:"merges_url"`
-    Archive_url string `json:"archive_url"`
-    Downloads_url string `json:"downloads_url"`
-    Issues_url string `json:"issues_url"`
-    Pulls_url string `json:"pulls_url"`
-    Milestones_url string `json:"milestones_url"`
-    Notifications_url string `json:"notifications_url"`
-    Labels_url string `json:"labels_url"`
-    Releases_url string `json:"releases_url"`
-    Created_at string `json:"created_at"`
-    Updated_at string `json:"updated_at"`
-    Pushed_at string `json:"pushed_at"`
-    Git_url string `json:"git_url"`
-    Ssh_url string `json:"ssh_url"`
-    Clone_url string `json:"clone_url"`
-    Svn_url string `json:"svn_url"`
+    ForksUrl string `json:"forks_url"`
+    KeysUrl string `json:"keys_url"`
+    CollaboratorsUrl string `json:"collaborators_url"`
+    TeamsUrl string `json:"teams_url"`
+    HooksUrl string `json:"hooks_url"`
+    IssueEvents_url string `json:"issue_events_url"`
+    EventsUrl string `json:"events_url"`
+    AssigneesUrl string `json:"assignees_url"`
+    BranchesUrl string `json:"branches_url"`
+    TagsUrl string `json:"tags_url"`
+    BlobsUrl string `json:"blobs_url"`
+    GitTags_url string `json:"git_tags_url"`
+    GitRefs_url string `json:"git_refs_url"`
+    TreesUrl string `json:"trees_url"`
+    StatusesUrl string `json:"statuses_url"`
+    LanguagesUrl string `json:"languages_url"`
+    StargazersUrl string `json:"stargazers_url"`
+    ContributorsUrl string `json:"contributors_url"`
+    SubscribersUrl string `json:"subscribers_url"`
+    SubscriptionUrl string `json:"subscription_url"`
+    CommitsUrl string `json:"commits_url"`
+    GitCommits_url string `json:"git_commits_url"`
+    CommentsUrl string `json:"comments_url"`
+    IssueComment_url string `json:"issue_comment_url"`
+    ContentsUrl string `json:"contents_url"`
+    CompareUrl string `json:"compare_url"`
+    MergesUrl string `json:"merges_url"`
+    ArchiveUrl string `json:"archive_url"`
+    DownloadsUrl string `json:"downloads_url"`
+    IssuesUrl string `json:"issues_url"`
+    PullsUrl string `json:"pulls_url"`
+    MilestonesUrl string `json:"milestones_url"`
+    NotificationsUrl string `json:"notifications_url"`
+    LabelsUrl string `json:"labels_url"`
+    ReleasesUrl string `json:"releases_url"`
+    CreatedAt string `json:"created_at"`
+    UpdatedAt string `json:"updated_at"`
+    PushedAt string `json:"pushed_at"`
+    GitUrl string `json:"git_url"`
+    SshUrl string `json:"ssh_url"`
+    CloneUrl string `json:"clone_url"`
+    SvnUrl string `json:"svn_url"`
     Homepage string `json:"homepage"`
     Size int `json:"size"`
-    Stargazers_count int `json:"stargazers_count"`
-    Watchers_count int `json:"watchers_count"`
+    StargazersCount int `json:"stargazers_count"`
+    WatchersCount int `json:"watchers_count"`
     Language string `json:"language"`
-    Has_issues bool `json:"has_issues"`
-    Has_downloads bool `json:"has_downloads"`
-    Has_wiki bool `json:"has_wiki"`
-    Forks_count int `json:"forks_count"`
-    Mirror_url string `json:"mirror_url"`
-    Open_issues_count string `json:"open_issues_count"`
+    HasIssues bool `json:"has_issues"`
+    HasDownloads bool `json:"has_downloads"`
+    HasWiki bool `json:"has_wiki"`
+    ForksCount int `json:"forks_count"`
+    MirrorUrl string `json:"mirror_url"`
+    OpenIssues_count string `json:"open_issues_count"`
     Forks int `json:"forks"`
-    Open_issues int `json:"open_issues"`
+    OpenIssues int `json:"open_issues"`
     Watchers int `json:"watchers"`
-    Default_branch string `json:"default_branch"`
+    DefaultBranch string `json:"default_branch"`
 }
 
 type GhRepoList struct {
